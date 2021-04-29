53d6810e79457347c0a68aab1fed6f7121767bab
Dillonhayes18 <g00373320@gmit.ie>
2021-04-29 23:21:36 +0100

    Working file reading

--- Project.py
+++ Project.py
@@ -1,17 +1,16 @@
 import argparse
+
 parser = argparse.ArgumentParser(description='Enter Regular Expression & File Path:')
 parser.add_argument('regexp', metavar='expression', type=str,
                     help='a string for the search')
-
 parser.add_argument('filepath', metavar='path', type=str,
                     help='a filepath for the search')
 args = parser.parse_args()
 
+fd=open(args.filepath)
+for i, line in enumerate(fd):
+      print(line)
 
-for i, line in enumerate(open(args.filepath)):
-      print(i)
-
-
-
+fd.close()
 print(args.regexp)
 print(args.filepath)
