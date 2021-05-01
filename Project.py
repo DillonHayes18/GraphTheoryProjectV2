7d53b34e6b5053b60563ce7568d5256f5265666e
Dillonhayes18 <g00373320@gmit.ie>
2021-05-01 17:53:31 +0100

    Added Comments

--- Project.py
+++ Project.py
@@ -1,4 +1,15 @@
 import argparse
+# Graph Theory Project 
+# Dillon Hayes - G00373320
+# April 2021
+
+# Program to take a regular expression and the name or path of the file 
+# as command line arguments and output the lines of the file matching the regular expression.
+
+
+# User prompt for adding inputing Regular Expression & File Path
+# Stores input as rexep which is type string
+# Stores input as filepath which is type string
 
 parser = argparse.ArgumentParser(description='Enter Regular Expression & File Path:')
 parser.add_argument('regexp', metavar='expression', type=str,
@@ -7,8 +18,12 @@ parser.add_argument('filepath', metavar='path', type=str,
                     help='a filepath for the search')
 args = parser.parse_args()
 
-with open(args.filepath, 'r') as searchfile:
-    for line in searchfile:
+# Opens the filepath and stores it as fileSearcher
+# for every line that has fileSearcher
+# if the regexp is in that line, print the line
+# close the filepath
+with open(args.filepath, 'r') as fileSearcher:
+    for line in fileSearcher:
         if args.regexp in line:
             print (line)
-searchfile.close()
+fileSearcher.close()
